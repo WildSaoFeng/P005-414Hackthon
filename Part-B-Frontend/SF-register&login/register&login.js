7661e525940c697b428b44d1a38d11258bde21ba
WildSaoFeng <1240730274@qq.com>
2018-04-14 22:57:46 +0800

    04-14 22:57:54 SF Login&Register 65% done

---
+++ Part-B-Frontend/SF-register&login/register&login.js
@@ -0,0 +1,9 @@
+function submitLogin() {
+    var username = document.forms['loginForm']['username'].value;
+    var password = document.forms['loginForm']['password'].value;
+
+}
+
+function submitRegister() {
+    
+}
